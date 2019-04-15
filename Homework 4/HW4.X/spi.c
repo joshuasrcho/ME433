88b1c9bd207bc0ea45c3144ebffc83d13943ae77
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-04-15 00:32:10 -0500

    wrote main function

--- Homework 4/HW4.X/spi.c
+++ Homework 4/HW4.X/spi.c
@@ -2,8 +2,10 @@
 #include "spi.h"
 
 void initSPI1(){
-    RPA1Rbits.RPA1R = 0b0011; // SDO1 assigned to pin A1.
-    RPB14Rbits.RPB14R = 0b0011; // SS1 assigned to pin B14
+    RPA1Rbits.RPA1R = 0b0011;  // SDO1 assigned to pin A1.
+    
+    TRISBbits.TRISB14 = 0;  // Pin B14 set as digital output which will act as manual SS1
+    CS = 1; 
     
     SPI1BUF;                  // clear the rx buffer by reading from it
     SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
@@ -12,9 +14,25 @@ void initSPI1(){
     SPI1CONbits.MSTEN = 1;    // master operation
     
     SPI1CONbits.ON = 1; // turn on SPI1 
-    
-  
-  
 }
 
-char SPI1_IO(char write);
+// send a byte via spi and return the response
+char SPI1_IO(char write){
+  SPI1BUF = write;
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+  return SPI1BUF;
+}
+
+void setVoltage(char channel, int voltage) {
+	unsigned short t = 0;
+	t= channel << 15; //a is at the very end of the data transfer
+	t = t | 0b01110000000000000;
+	t = t | ((voltage&0b1111111111) <<2); //rejecting excessive bits (above 10)
+	
+	CS = 0;
+	spi_io(t>>8);
+	spi_
+	
+}
--- Homework 4/HW4.X/spi.h
+++ Homework 4/HW4.X/spi.h
@@ -1,6 +1,8 @@
 #ifndef SPI_H
 #define	SPI_H
 
+#define CS LATBbits.LATB14 // chip select pin
+
 void initSPI1();
 char SPI1_IO(char write);
 
