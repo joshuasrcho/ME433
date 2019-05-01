fa63d495c41aac25fb2c2fa6fffe4b5293193617
Joshua Cho <seungcho2021@u.northwestern.edu>
2019-05-01 12:03:18 -0500

    recreated homework 7 using harmony

--- Homework 8/HW8/firmware/src/app.c
+++ Homework 8/HW8/firmware/src/app.c
@@ -54,6 +54,9 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 
 #include "app.h"
+#include "i2c_master_noint.h"
+#include "ili9341.h"
+#include <stdio.h>
 
 // *****************************************************************************
 // *****************************************************************************
@@ -96,6 +99,13 @@ APP_DATA appData;
 
 /* TODO:  Add any necessary local functions.
 */
+#define LSM6DS33_ADDR 0x6B // device op code
+
+void initIMU(void);
+void I2C_read_multiple(unsigned char addr, unsigned char rgstr, unsigned char * data, int lnt);
+void LCD_writeCharacter(unsigned short x, unsigned short y, char c);
+void LCD_writeString(unsigned short x, unsigned short y, char* m);
+void LCD_IMUBar(unsigned short x, unsigned short y, int accelX, int accelY);
 
 
 // *****************************************************************************
@@ -135,13 +145,33 @@ void APP_Initialize ( void )
 
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
-
-    // do your TRIS and LAT commands here
+    
     TRISBbits.TRISB4 = 1; // B4 (pushbutton) is an input pin
     TRISAbits.TRISA4 = 0; // A4 (green LED) is an output pin
-    LATAbits.LATA4 = 1; // A4 is HIGH (green LED on)
-
+    LATAbits.LATA4 = 0; // A4 is LOW (green LED off)
+    
     __builtin_enable_interrupts();
+    
+    // setup and initialization
+    i2c_master_setup();                       // initialize I2C2, which we use as a master
+    initIMU();                           // initialize LSM6DS33 IMU chip
+    SPI1_init();
+    LCD_init();
+    LCD_clearScreen(ILI9341_YELLOW);
+    
+    // actual code
+    char message[100];
+    char whoami;
+    
+    // test read from WHO_AM_I register
+    i2c_master_start();
+    i2c_master_send(LSM6DS33_ADDR << 1); // write
+    i2c_master_send(0x0F); // send address of the first register to read from
+    i2c_master_restart();
+    i2c_master_send((LSM6DS33_ADDR << 1) | 1); // read
+    whoami = i2c_master_recv();
+    sprintf(message,"WHOAMI: %d",whoami);
+    LCD_writeString(150,20,message);
 }
 
 
@@ -175,14 +205,28 @@ void APP_Tasks ( void )
 
         case APP_STATE_SERVICE_TASKS:
         {
+            unsigned char d[15];
+            char message[100];
+            I2C_read_multiple(LSM6DS33_ADDR, 0x20, d, 14);
+            short temperature = (d[0] << 8) | d[1]; // shift high byte and OR with low byte 
+            short gyroX = (d[2] << 8) | d[3];
+            short gyroY = (d[4] << 8) | d[5];
+            short gyroZ = (d[6] << 8) | d[7];
+            short accelX = (d[8] << 8) | d[9];
+            short accelY = (d[10] << 8) | d[11];
+            short accelZ = (d[12] << 8) | d[13];
+            sprintf(message,"accelX:%6d",accelX);
+            LCD_writeString(20,20,message);
+            sprintf(message,"accelY:%6d",accelY);
+            LCD_writeString(20,40,message);
+        
+            LCD_IMUBar(120,160,accelX,accelY);
+        
             _CP0_SET_COUNT(0);
-            while(_CP0_GET_COUNT() <= 12000){
-                ; // delay for 0.5 milliseconds. Core timer runs at 24 MHz.
+            while(_CP0_GET_COUNT() < 1200000){
+                ;
             }
-            LATAbits.LATA4 = !LATAbits.LATA4; // toggle green LED
-            while (!(PORTBbits.RB4)){
-                LATAbits.LATA4 = 0; // turn off green LED while button is pressed
-            }
+            LATAbits.LATA4 = !LATAbits.LATA4;
             break;
         }
 
@@ -198,7 +242,104 @@ void APP_Tasks ( void )
     }
 }
 
- 
+
+void initIMU(){
+    i2c_master_start();
+    i2c_master_send(LSM6DS33_ADDR << 1);
+    i2c_master_send(0x10); // write to CTRL1_XL register
+    i2c_master_send(0x82); // 1.66kHz sample rate, 2g sensitivity, 100Hz filter
+    i2c_master_restart();
+    i2c_master_send(LSM6DS33_ADDR << 1);
+    i2c_master_send(0x11); // write to CTRL2_G register
+    i2c_master_send(0x88); // 1.66kHz sample rate, 1000 dps sensitivity
+    i2c_master_send(LSM6DS33_ADDR << 1);
+    i2c_master_send(0x12); // write to CTRL3_C register
+    i2c_master_send(0x4); // turn on IF_INC
+    i2c_master_stop();
+    
+}
+
+void I2C_read_multiple(unsigned char addr, unsigned char rgstr, unsigned char * data, int lnt){
+    int i;
+    i2c_master_start();
+    i2c_master_send(addr << 1); // write
+    i2c_master_send(rgstr); // send address of the first register to read from
+    i2c_master_restart();
+    i2c_master_send((addr << 1) | 1); // read
+    
+    for (i = 0; i<lnt; i++){
+        data[i] = i2c_master_recv();
+        if (i == (lnt-1)){
+            i2c_master_ack(1);
+        }
+        else{
+            i2c_master_ack(0);
+        }
+    }
+    i2c_master_stop(); 
+}
+
+void LCD_writeCharacter(unsigned short x, unsigned short y, char c){
+    int i, j;
+    if (x <= 235 && y <= 315){
+        for (i=0; i<5; i++){
+            for (j=0; j<8; j++){
+                char t = ASCII[c-32][i] >> (7-j);
+                t = t & 0x1;
+                if (t){
+                    LCD_drawPixel(x+i,y-j,ILI9341_PURPLE);
+                }
+                else{
+                    LCD_drawPixel(x+i,y-j,ILI9341_YELLOW );
+                }
+            }
+        }    
+    }
+    else{;}
+}
+
+void LCD_writeString(unsigned short x, unsigned short y, char* m){
+    int i = 0; 
+    while(m[i]){ 
+        LCD_writeCharacter(x+6*i, y, m[i]); 
+        i++;
+    }
+}
+
+void LCD_IMUBar(unsigned short x, unsigned short y, int accelX, int accelY){
+    int i, j;
+    // X direction
+    for (i = 0; i < 100; i++){
+        for (j = 0; j < 5; j++){
+            if (i < (accelX/100)){
+               LCD_drawPixel(x+i,y+j,ILI9341_RED); 
+            }
+            else{
+                LCD_drawPixel(x+i,y+j,ILI9341_LIGHTGREY);
+            }
+            if (-i > (accelX/100)){
+                LCD_drawPixel(x-i,y+j,ILI9341_RED); 
+            }
+            else{
+                LCD_drawPixel(x-i,y+j,ILI9341_LIGHTGREY);
+            }
+            // Y direction
+            if (i < (accelY/100)){
+               LCD_drawPixel(x+j,y+i,ILI9341_RED); 
+            }
+            else{
+                LCD_drawPixel(x+j,y+i,ILI9341_LIGHTGREY);
+            }
+            if (-i >= (accelY/100)){
+                LCD_drawPixel(x+j,y-i,ILI9341_RED); 
+            }
+            else{
+                LCD_drawPixel(x+j,y-i,ILI9341_LIGHTGREY);
+            }
+        }
+    }
+  
+}
 
 /*******************************************************************************
  End of File
